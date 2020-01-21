b660efab3ed974f61df9c424ef4639f417bf1a7a
mfadin <m.fadin@ibm.com>
2020-01-22 00:17:10 +0400

    updated to version 2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ http = require("http");
 http
   .createServer(function(req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("Hello! I have been deployed by Tekton -- v1");
+    res.write("Hello! I have been deployed by Tekton -- v2");
     res.end();
   })
   .listen(8080);
